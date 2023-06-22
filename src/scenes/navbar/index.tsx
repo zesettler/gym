5d43e2adf3d5a08e7feedb3809764a22487cb7d3
zesettler <gromovgrin@gmail.com>
2023-06-22 21:26:01 +0300

    navbar

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+
+
+const Navbar = () => {
+  
+
+  return (
+    <div>
+        Navbar
+    </div>
+  );
+};
+
+export default Navbar;
