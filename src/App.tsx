7e6adb2d0806338a494ac0e5a4705355704b6a2d
zesettler <gromovgrin@gmail.com>
2023-06-22 23:04:05 +0300

    navbar fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Navbar from "@/scenes/navbar";
+import { useState } from "react"
+import Navbar from "./scenes/navbar/Navbar"
+
+enum SelectedPage {
+  Home = 'home',
+  Benefits = 'benefits',
+  OurClasses = 'ourClasses',
+  ContactUs = 'contactus'
+}
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <div className='app bg-gray-20'>
-      <Navbar />
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
